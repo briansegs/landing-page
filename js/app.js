d62aa35b55f929a5345567ca8562dcbc30e9e786
brian <briansegs@gmail.com>
2021-03-02 16:42:44 -0500

    style: Remove comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
-*/
-
-/**
- * Define Global Variables
- *
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
 const sections = document.querySelectorAll('section');
 
 function scrollToDiv(id) {
@@ -63,31 +37,6 @@ for (let section of sections) {
 window.addEventListener('scroll', toggleActiveDiv);
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
 
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
