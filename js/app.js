5a50ac63edd6423cfad0227d187475c58e07eed7
brian <briansegs@gmail.com>
2021-03-02 13:00:38 -0500

    feat: Add set active on scroll

--- js/app.js
+++ js/app.js
@@ -27,20 +27,14 @@
 let sections = document.querySelectorAll('section');
 let nav = document.getElementById('navbar__list');
 
-let set = function (evt) {
-    // let elmnt = document.getElementById(evt.dataset.id);
-    // elmnt.classList.add('your-active-class');
-    console.log(evt);
-    return evt
-};
+
 
 for (let section of sections) {
     let li = document.createElement('li');
     let name = section.id;
-    li.setAttribute('class', 'menu__link')
 
-    li.innerText = section.dataset.nav;
-    li.addEventListener( 'click', set)
+	li.setAttribute('class', 'menu__link')
+	li.innerText = section.dataset.nav;
     li.addEventListener('click', function () {
       let e = document.getElementById(name);
       e.scrollIntoView({
@@ -53,24 +47,20 @@ for (let section of sections) {
     nav.appendChild(li);
 };
 
-window.addEventListener('scroll', function () {
-    let actBox = document.querySelector('section.your-active-class');
-    let top = actBox.getBoundingClientRect().top;
-    let bottom = actBox.getBoundingClientRect().bottom;
-    let winHeight = window.innerHeight;
-    if (top < winHeight)
-    {
-        actBox.classList.add('your-active-class');
-        console.log(top)
-        // console.log(section.getBoundingClientRect().bottom)
-        console.log(winHeight)
 
-    }
-    // if (bottom > winHeight)
-    // {
-    //     actBox.classList.remove('your-active-class');
-    // }
-    });
+window.addEventListener('scroll', function () {
+	let current = document.getElementsByClassName("your-active-class");
+	for (section of sections) {
+		let fullView = section.getBoundingClientRect().top + section.getBoundingClientRect().bottom;
+		if (fullView < window.innerHeight)
+		{
+			current[0].className = current[0].className.replace("your-active-class", "");
+			section.className += "your-active-class";
+		}
+	}
+});
+
+
 /**
  * End Helper Functions
  * Begin Main Functions
