ce9d92dd460bf687f0aa01a9f6bff928885d245b
brian <briansegs@gmail.com>
2021-03-02 16:55:57 -0500

    refactor: Switch let to const

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('section');
 
 function scrollToDiv(id) {
-	let div = document.getElementById(id);
+	const div = document.getElementById(id);
 	div.scrollIntoView({
 		block: 'start',
 		behavior: 'smooth',
@@ -10,10 +10,10 @@ function scrollToDiv(id) {
 };
 
 function toggleActiveDiv() {
-	let active = "your-active-class";
+	const active = "your-active-class";
 	let current = document.getElementsByClassName(active);
 	for (section of sections) {
-			let wholeDiv = section.getBoundingClientRect().top + section.getBoundingClientRect().bottom;
+			const wholeDiv = section.getBoundingClientRect().top + section.getBoundingClientRect().bottom;
 		if (wholeDiv < window.innerHeight) {
 			current[0].classList.remove(active);
 			section.classList.add(active);
@@ -23,9 +23,9 @@ function toggleActiveDiv() {
 
 for (let section of sections) {
     let li = document.createElement('li');
-    let id = section.id;
-	let sectionName = section.dataset.nav;
-	let navBar = document.getElementById('navbar__list');
+    const id = section.id;
+	const sectionName = section.dataset.nav;
+	const navBar = document.getElementById('navbar__list');
 	li.setAttribute('class', 'menu__link');
 	li.innerText = sectionName;
     li.addEventListener('click', function () {
