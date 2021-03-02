752be230dde9627e803f75ad43eb36984b2e3752
brian <briansegs@gmail.com>
2021-03-02 09:25:38 -0500

    feat: Add scroll to nav bar

--- js/app.js
+++ js/app.js
@@ -24,25 +24,53 @@
  * Start Helper Functions
  *
 */
-
-let scroll = function () {
-    let elmnt = document.querySelector('.your-active-class');
-    elmnt.scrollIntoView({
-        behavior: "smooth",
-    });
-}
-
 let sections = document.querySelectorAll('section');
 let nav = document.getElementById('navbar__list');
-for (let section of sections) {
-    let li = document.createElement('li');
-    li.setAttribute('class', 'menu__link')
-    li.innerText = section.dataset.nav;
-    nav.appendChild(li);
-    li.addEventListener('click', scroll)
+
+let set = function (evt) {
+    // let elmnt = document.getElementById(evt.dataset.id);
+    // elmnt.classList.add('your-active-class');
+    console.log(evt);
+    return evt
 };
 
+for (let section of sections) {
+    let li = document.createElement('li');
+    let name = section.id;
+    li.setAttribute('class', 'menu__link')
 
+    li.innerText = section.dataset.nav;
+    li.addEventListener( 'click', set)
+    li.addEventListener('click', function () {
+      let e = document.getElementById(name);
+      e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+      });
+    })
+
+    nav.appendChild(li);
+};
+
+window.addEventListener('scroll', function () {
+    let actBox = document.querySelector('section.your-active-class');
+    let top = actBox.getBoundingClientRect().top;
+    let bottom = actBox.getBoundingClientRect().bottom;
+    let winHeight = window.innerHeight;
+    if (top < winHeight)
+    {
+        actBox.classList.add('your-active-class');
+        console.log(top)
+        // console.log(section.getBoundingClientRect().bottom)
+        console.log(winHeight)
+
+    }
+    // if (bottom > winHeight)
+    // {
+    //     actBox.classList.remove('your-active-class');
+    // }
+    });
 /**
  * End Helper Functions
  * Begin Main Functions
