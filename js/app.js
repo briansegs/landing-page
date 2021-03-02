37a9842ce0dc216519c711d9bd682f0ea1a2b024
brian <briansegs@gmail.com>
2021-03-02 17:52:56 -0500

    style: Update to style guide

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 // Global varisbles
 
+/**
+* @description Stores all divs
+*/
 const sections = document.querySelectorAll('section');
 
 
 // Functions
 
-/*
-Function below will:
-Store a div by id
-Scroll the viewport to that div
+/**
+* @description Function below will:
+* Scroll the viewport to div by it's id
+* @param (div-id)
 */
 function scrollToDiv(id) {
 	const div = document.getElementById(id);
@@ -17,14 +20,14 @@ function scrollToDiv(id) {
 		behavior: 'smooth',
 		inline: 'start'
 	});
-};
+}
 
-/*
-Function below will:
-Keep track of the current active div
-Loop through all divs to find which div is in view
-Remove active state from stored div
-Add active state to the div that is in view
+/**
+* @description Function below will:
+* Keep track of the current active div
+* Loop through all divs to find which div is in view
+* Remove active state from stored div
+* Add active state to the div that is in view
 */
 function toggleActiveDiv() {
 	const active = "your-active-class";
@@ -41,14 +44,14 @@ function toggleActiveDiv() {
 
 // Navigation bar setup
 
-/*
-The 'for of' loop below will:
-Loop through all divs
-Create a list Item
-Set the list item's class to 'menu__Link'
-Add the div's name to it's innerText
-Add an EventListener with scrollToDive attached to it
-Append the List item to the navigation bar
+/**
+* @description The 'for of' loop below will:
+* Loop through all divs
+* Create a list Item
+* Set the list item's class to 'menu__Link'
+* Add the div's name to it's innerText
+* Add an EventListener with scrollToDive attached to it
+* Append the List item to the navigation bar
 */
 for (let section of sections) {
     let li = document.createElement('li');
@@ -66,9 +69,9 @@ for (let section of sections) {
 
 // Set active div
 
-/*
-Listens for when a div is scrolled to
-Sets div to active
+/**
+* @description Listens for when a div is scrolled to
+* Sets div to active
 */
 window.addEventListener('scroll', toggleActiveDiv);
 
